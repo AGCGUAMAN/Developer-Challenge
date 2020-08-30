3a7c910c8f6281a383426b52a1955897ce3824b1
ALEXANDER GUAMAN CRUZ <alexander.guaman.cruz@gmail.com>
2020-08-29 22:11:00 -0500

    My first commit

---
+++ Developer_challenge.js
@@ -0,0 +1,9 @@
+var nombre = 'Alexander', primerApellido = 'Guamán', segundoApellido = 'Cruz'
+var edad = 44
+
+edad = '44 años'
+
+console.log(`Hola ${nombre} ${primerApellido} ${segundoApellido}`)
+console.log(`Tengo ${edad}`)
+
+var peso = 70
